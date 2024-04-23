0ea543bdb3476ade95fb828fd6986859f12a7524
Nadiia <nadink1603@gmail.com>
2024-04-23 15:45:00 +0200

    Add project files.

---
+++ FrmTicTacToe.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace TicTacToe
+{
+    partial class FrmTicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelCell0_0 = new System.Windows.Forms.Panel();
+            this.panelCell0_1 = new System.Windows.Forms.Panel();
+            this.panelCell0_2 = new System.Windows.Forms.Panel();
+            this.panelCell1_0 = new System.Windows.Forms.Panel();
+            this.panelCell1_1 = new System.Windows.Forms.Panel();
+            this.panelCell1_2 = new System.Windows.Forms.Panel();
+            this.panelCell2_0 = new System.Windows.Forms.Panel();
+            this.panelCell2_1 = new System.Windows.Forms.Panel();
+            this.panelCell2_2 = new System.Windows.Forms.Panel();
+            this.labelNewGameTitle = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panelCell0_0
+            // 
+            this.panelCell0_0.BackColor = System.Drawing.Color.Indigo;
+            this.panelCell0_0.Location = new System.Drawing.Point(12, 12);
+            this.panelCell0_0.Name = "panelCell0_0";
+            this.panelCell0_0.Size = new System.Drawing.Size(99, 96);
+            this.panelCell0_0.TabIndex = 0;
+            // 
+            // panelCell0_1
+            // 
+            this.panelCell0_1.BackColor = System.Drawing.Color.Indigo;
+            this.panelCell0_1.Location = new System.Drawing.Point(117, 12);
+            this.panelCell0_1.Name = "panelCell0_1";
+            this.panelCell0_1.Size = new System.Drawing.Size(99, 96);
+            this.panelCell0_1.TabIndex = 1;
+            // 
+            // panelCell0_2
+            // 
+            this.panelCell0_2.BackColor = System.Drawing.Color.Indigo;
+            this.panelCell0_2.Location = new System.Drawing.Point(222, 12);
+            this.panelCell0_2.Name = "panelCell0_2";
+            this.panelCell0_2.Size = new System.Drawing.Size(99, 96);
+            this.panelCell0_2.TabIndex = 2;
+            // 
+            // panelCell1_0
+            // 
+            this.panelCell1_0.BackColor = System.Drawing.Color.Indigo;
+            this.panelCell1_0.Location = new System.Drawing.Point(12, 114);
+            this.panelCell1_0.Name = "panelCell1_0";
+            this.panelCell1_0.Size = new System.Drawing.Size(99, 96);
+            this.panelCell1_0.TabIndex = 3;
+            // 
+            // panelCell1_1
+            // 
+            this.panelCell1_1.BackColor = System.Drawing.Color.Indigo;
+            this.panelCell1_1.Location = new System.Drawing.Point(117, 114);
+            this.panelCell1_1.Name = "panelCell1_1";
+            this.panelCell1_1.Size = new System.Drawing.Size(99, 96);
+            this.panelCell1_1.TabIndex = 4;
+            // 
+            // panelCell1_2
+            // 
+            this.panelCell1_2.BackColor = System.Drawing.Color.Indigo;
+            this.panelCell1_2.Location = new System.Drawing.Point(222, 114);
+            this.panelCell1_2.Name = "panelCell1_2";
+            this.panelCell1_2.Size = new System.Drawing.Size(99, 96);
+            this.panelCell1_2.TabIndex = 5;
+            // 
+            // panelCell2_0
+            // 
+            this.panelCell2_0.BackColor = System.Drawing.Color.Indigo;
+            this.panelCell2_0.Location = new System.Drawing.Point(12, 216);
+            this.panelCell2_0.Name = "panelCell2_0";
+            this.panelCell2_0.Size = new System.Drawing.Size(99, 96);
+            this.panelCell2_0.TabIndex = 6;
+            // 
+            // panelCell2_1
+            // 
+            this.panelCell2_1.BackColor = System.Drawing.Color.Indigo;
+            this.panelCell2_1.Location = new System.Drawing.Point(117, 216);
+            this.panelCell2_1.Name = "panelCell2_1";
+            this.panelCell2_1.Size = new System.Drawing.Size(99, 96);
+            this.panelCell2_1.TabIndex = 7;
+            // 
+            // panelCell2_2
+            // 
+            this.panelCell2_2.BackColor = System.Drawing.Color.Indigo;
+            this.panelCell2_2.Location = new System.Drawing.Point(222, 216);
+            this.panelCell2_2.Name = "panelCell2_2";
+            this.panelCell2_2.Size = new System.Drawing.Size(99, 96);
+            this.panelCell2_2.TabIndex = 8;
+            // 
+            // labelNewGameTitle
+            // 
+            this.labelNewGameTitle.AutoSize = true;
+            this.labelNewGameTitle.Font = new System.Drawing.Font("Franklin Gothic Medium", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNewGameTitle.ForeColor = System.Drawing.Color.White;
+            this.labelNewGameTitle.Location = new System.Drawing.Point(359, 71);
+            this.labelNewGameTitle.Name = "labelNewGameTitle";
+            this.labelNewGameTitle.Size = new System.Drawing.Size(219, 47);
+            this.labelNewGameTitle.TabIndex = 9;
+            this.labelNewGameTitle.Text = "New Game:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Franklin Gothic Medium", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(327, 255);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 38);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Player: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(514, 296);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(462, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(376, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(472, 114);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(379, 156);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 16);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "label7";
+            // 
+            // FrmTicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(585, 327);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelNewGameTitle);
+            this.Controls.Add(this.panelCell2_2);
+            this.Controls.Add(this.panelCell2_1);
+            this.Controls.Add(this.panelCell2_0);
+            this.Controls.Add(this.panelCell1_2);
+            this.Controls.Add(this.panelCell1_1);
+            this.Controls.Add(this.panelCell1_0);
+            this.Controls.Add(this.panelCell0_2);
+            this.Controls.Add(this.panelCell0_1);
+            this.Controls.Add(this.panelCell0_0);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmTicTacToe";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TicTacToe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelCell0_0;
+        private System.Windows.Forms.Panel panelCell0_1;
+        private System.Windows.Forms.Panel panelCell0_2;
+        private System.Windows.Forms.Panel panelCell1_0;
+        private System.Windows.Forms.Panel panelCell1_1;
+        private System.Windows.Forms.Panel panelCell1_2;
+        private System.Windows.Forms.Panel panelCell2_0;
+        private System.Windows.Forms.Panel panelCell2_1;
+        private System.Windows.Forms.Panel panelCell2_2;
+        private System.Windows.Forms.Label labelNewGameTitle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
