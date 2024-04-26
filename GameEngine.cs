ca5c720f7b6fcff676d14ee070bbda591569ff2b
Nadiia <nadink1603@gmail.com>
2024-04-26 16:31:33 +0200

    added properties and enum to GameEngine

--- GameEngine.cs
+++ GameEngine.cs
@@ -1,6 +1,43 @@
-﻿namespace TicTacToe
+﻿using System;
+using System.Drawing;
+
+namespace TicTacToe
 {
     internal class GameEngine
     {
+        internal enum GameMode
+        {
+            None,
+            PlayerVsPlayer,
+            PlayerVsCPU
+        }
+
+        internal enum WhooseTurn
+        {
+            Player1Human,
+            Player2Human,
+            Player2CPU
+        }
+
+        private GameMode Mode {  get; set; } = GameMode.None;
+        private WhooseTurn Turn { get; set; } = WhooseTurn.Player1Human;
+        private string Winner { get; set; } = "";
+        
+        private int player1Score = 0;
+        private int player2Score = 0;
+        private int numberOfDraws = 0;
+
+        const char EMPTY_CELL = '-';
+        const char X_MARK = 'X';
+        const char O_MARK = 'O';
+
+        public const string PLAYER_HUMAN_TITLE = "Player";
+        public const string PLAYER_CPU_TITLE = "Computer";
+
+        private char[][] gameField = new char[][] {
+            new char[] { EMPTY_CELL, EMPTY_CELL, EMPTY_CELL },
+            new char[] { EMPTY_CELL, EMPTY_CELL, EMPTY_CELL },
+            new char[] { EMPTY_CELL, EMPTY_CELL, EMPTY_CELL }
+        };
     }
 }
